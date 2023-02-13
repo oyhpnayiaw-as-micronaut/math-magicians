28c32d375befa0849a7ecb06da74cf0666c923c4
EBrain <ebrain.ebrain@gmail.com>
2023-02-13 17:50:56 +0630

    Finish Calculator component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import './Calculator.css';
 
-const Calculator = (props) => {
-  console.log(props);
+const Calculator = () => {
+  const buttons = [
+    'AC',
+    '+/-',
+    '%',
+    '÷',
+    '7',
+    '8',
+    '9',
+    '×',
+    '4',
+    '5',
+    '6',
+    '-',
+    '1',
+    '2',
+    '3',
+    '+',
+    '0',
+    '.',
+    '=',
+  ];
 
   return (
-    <div>
-      <h1>Calculator</h1>
+    <div className="calculator">
+      <div className="calculator-inner">
+        <input type="text" placeholder="0" className="calculator-input" />
+        {buttons.map((button) => (
+          <button key={button} type="button" className="calculator-button">
+            {button}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
