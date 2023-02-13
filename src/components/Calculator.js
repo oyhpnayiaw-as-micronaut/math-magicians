fdb28d9a799ee483d9412efe09520c109338cd52
EBrain <ebrain.ebrain@gmail.com>
2023-02-13 17:19:58 +0630

    Remove boilerplates from create-react-app

---
+++ src/components/Calculator.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Calculator.css';
+
+const Calculator = (props) => {
+  console.log(props);
+
+  return (
+    <div>
+      <h1>Calculator</h1>
+    </div>
+  );
+};
+
+export default Calculator;
